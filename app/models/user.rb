781755219266fc2a8157635a452be28e350368c0
anakettl <anakettl@gmail.com>
2022-04-30 12:45:59 -0300

    feat(user): adicionar role com as opcoes de terapeuta e paciente

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { therapist: 0, patient: 1 }
 end
