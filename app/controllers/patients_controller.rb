d4a3d381809ed11cf2628c402f814782d1cba643
anakettl <anakettl@gmail.com>
2022-04-30 12:48:32 -0300

    feat(paciente): adicionar criacao do paciente no banco e refatorar a view de pacientes para receber dados de login e criar o usuario com a role de paciente no mesmo fluxo

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,5 +3,26 @@ class PatientsController < ApplicationController
   end
 
   def new
+    @patient = Patient.new
+    @user = User.new
+  end
+
+  def create
+    if Patient.create(patient_params) && User.create(user_params)
+      redirect_to patients_path
+    else
+      redirect_to new_patient_path
+    end
+  end
+
+  def index
+  end
+
+  def patient_params
+    params.require(:patient).permit(:name, :dt_nasc, :dt_atend, :resume, :phone)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :role)
   end
 end
