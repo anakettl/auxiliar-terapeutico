5839131eacddb8263b14567bd7666949550a7a56
anakettl <anakettl@gmail.com>
2022-06-27 20:39:00 -0300

    feat(exercise): aceitar formato de video .mov

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -6,6 +6,6 @@ class Exercise < ApplicationRecord
   has_one_attached :video
 
   validates :video,
-            content_type: [:mp4],
+            content_type: [:mp4, :mov],
             size: { less_than: 15.megabytes , message: 'O vídeo deve ter menos de 15MB' }
 end
